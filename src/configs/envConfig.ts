cd395cddd8e2947fb0dca23b5d51145c10c8da2f
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-02 21:21:59 +0530

    setting up auth middleware

--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const SERVER_PORT = process.env.SERVER_PORT
 export const DATABASE_URL = process.env.DATABASE_URL
+export const JWT_SECRET = process.env.JWT_SECRET
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,44 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../configs/envConfig.js'
+
+interface JwtPayload{
+    id: number;
+    role: string;
+}
+
+export const auth = (roles: string[] = []) => {
+    return(req: Request, res: Response, next: NextFunction) => {
+        try {
+            const token = req.headers.authorization?.split(" ")[1];
+            if(!token){
+                return res
+                    .status(401)
+                    .json({message: 'Not authorized, Token missing'});
+            }
+
+            const decoded = jwt.verify(token, JWT_SECRET!) as JwtPayload;
+            req.user = decoded;
+
+            if(roles.length && !roles.includes(decoded.role)){
+                return res
+                    .status(403)
+                    .json({message: "Forbidden"});
+            }
+
+            next();
+        }
+        catch (error) {
+            console.error(error);
+            if(error instanceof Error){
+                const errorMessage = error.message;
+                return res
+                    .status(400)
+                    .json({error: 'Invalid token', message: errorMessage});
+            }
+            return res
+                .status(500)
+                .json({error: 'Internal server error'});
+        }
+    }
+}
