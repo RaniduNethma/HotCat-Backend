9292c9c5c6da0bece5c1f345076052e668c96ad5
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-04 20:56:16 +0530

    user controller func name change

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import * as userServices from '../services/user.services.js';
 
-export const register = async(req: Request, res: Response, next: NextFunction) => {
+export const userRegisterController = async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const user = await userServices.register(req.body);
+        const user = await userServices.userRegisterService(req.body);
         res.status(200).json(user);
     }
     catch (error) {
