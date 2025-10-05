a0818de7a1c930beb273584638d24c0a103d7452
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-05 13:48:38 +0530

    create userLoginController with error handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -19,3 +19,21 @@ export const userRegisterController = async(req: Request, res: Response, next: N
             .json({error: 'Internal server error', message: error});
     }
 }
+
+export const userLoginController = async (req: Request, res: Response, next: NextFunction){
+    try {
+        
+    }
+    catch (error) {
+        console.error(error);
+        if(error instanceof Error){
+            const errorMessage = error.message;
+            return res
+                .status(400)
+                .json({error: 'Error executing query', message: errorMessage});
+        }
+        return res
+            .status(500)
+            .json({error: 'Internal server error', message: error});
+    }
+}
