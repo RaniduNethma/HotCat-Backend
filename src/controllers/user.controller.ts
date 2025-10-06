82a7831cc2720df728e6c63b1b59f57c5c77c33a
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-06 21:53:41 +0530

    serRegisterService update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import * as userServices from '../services/user.services.js';
 
 export const userRegisterController = async(req: Request, res: Response, next: NextFunction) => {
     try {
+        const { email, password, first_name, last_name } = req.body
         const user = await userServices.userRegisterService(req.body);
         res.status(200).json(user);
     }
@@ -22,15 +23,15 @@ export const userRegisterController = async(req: Request, res: Response, next: N
 
 export const userLoginController = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {email, password} = req.body;
+        const {userName, password} = req.body;
 
-        if(!email || !password){
+        if(!userName || !password){
             return res
                 .status(400)
-                .json({error: 'Email and password are required fields'});
+                .json({error: 'User name and password are required fields'});
         }
 
-        const {statusCode, message, user, token} = await userServices.userLoginService(email, password);
+        const {statusCode, message, user, token} = await userServices.userLoginService(userName, password);
 
         return res
             .status(statusCode)
