e15a8097383e4b7247049a7bba35b861e01cfe29
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-20 20:30:05 +0530

    remove authenticate

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -42,37 +42,3 @@ export const auth = (roles: string[] = []) => {
         }
     }
 }
-
-export const authenticate = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const authHeader = req.headers.authorization;
-
-    if(!authHeader){
-        return res
-            .status(401)
-            .json({message: 'Not authorized, Token missing'});
-    }
-
-    const token = authHeader.split(" ")[1];
-
-    try {
-        const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET!);
-        req.user = decoded;
-        next();
-    }
-    catch (error) {
-        console.error(error);
-            if(error instanceof Error){
-                const errorMessage = error.message;
-                return res
-                    .status(400)
-                    .json({error: 'Invalid token', message: errorMessage});
-            }
-            return res
-                .status(500)
-                .json({message: 'Internal server error'});
-    }
-}
