78a8b0283d9e0dd75092d499be6d64204399d27d
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-21 00:20:31 +0530

    env config update

--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -2,12 +2,30 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const SERVER_PORT = process.env.SERVER_PORT
-export const DATABASE_URL = process.env.DATABASE_URL
+interface EnvConfig {
+    SERVER_PORT: number;
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+    ACCESS_TOKEN_SECRET: string;
+    REFRESH_TOKEN_SECRET: string;
+    ACCESS_TOKEN_EXPIRES: number;
+    REFRESH_TOKEN_EXPIRES: number;
+}
 
-export const JWT_SECRET = process.env.JWT_SECRET
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET
+function getEnvVariable(key: any, required = true): any {
+  const value = process.env[key];
+  if (!value && required) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value as string;
+}
 
-export const ACCESS_TOKEN_EXPIRES = process.env.ACCESS_TOKEN_EXPIRES
-export const REFRESH_TOKEN_EXPIRES = process.env.REFRESH_TOKEN_EXPIRES
+export const env: EnvConfig = {
+  SERVER_PORT: Number(getEnvVariable("SERVER_PORT")),
+  DATABASE_URL: getEnvVariable("DATABASE_URL"),
+  JWT_SECRET: getEnvVariable("JWT_SECRET"),
+  ACCESS_TOKEN_SECRET: getEnvVariable("ACCESS_TOKEN_SECRET"),
+  REFRESH_TOKEN_SECRET: getEnvVariable("REFRESH_TOKEN_SECRET"),
+  ACCESS_TOKEN_EXPIRES: getEnvVariable("ACCESS_TOKEN_EXPIRES"),
+  REFRESH_TOKEN_EXPIRES: getEnvVariable("REFRESH_TOKEN_EXPIRES"),
+};
