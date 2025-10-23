7db831f933f8f1b42cb718bf2d235f0c84bc8e47
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-23 21:59:18 +0530

    update getAllUsers function with try catch block

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -6,19 +6,28 @@ export const getAllUsers = async (
     const limit: number = 10;
     const skip: number = (page - 1) * limit;
 
-    const allUsers = await DB.user.findMany({
-        take: limit,
-        skip,
-        select: {
-            id: true,
-            userName: true,
-            name: true,
-            role: true
-        }
-    });
+    try {
+        const allUsers = await DB.user.findMany({
+            take: limit,
+            skip,
+            select: {
+                id: true,
+                userName: true,
+                name: true,
+                role: true
+            }
+        });
 
-    return{
-        statusCode: 200,
-        data: allUsers
-    };
+        return{
+            statusCode: 200,
+            data: allUsers
+        };
+    }
+    catch (error) {
+        console.error("Error executing logout", error);
+        return {
+        statusCode: 500,
+        message: "Internal server error",
+        };
+    }
 }
