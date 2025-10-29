2c5dc5a9d75f818199eb1630a4e2fcda8f038d40
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-10-29 22:50:35 +0530

    create getUserHandler in user.controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -33,3 +33,35 @@ export const getAllUsersHandler = async (
             .json({ error: "Internal server error", message: error });
     }
 }
+
+export const getUserHandler = async (
+    req: Request,
+    res: Response
+)=> {
+    if(!req.user || req.user.id !== req.params.id) {
+        return res
+            .status(401)
+            .json({ error: "Not authorized to access this profile"});
+    }
+
+    try {
+        const {id} = req.params.id;
+
+        const {statusCode, data} = await userServices.getUserById(id);
+        return res
+            .status(statusCode)
+            .json({data});
+    }
+    catch (error) {
+        console.error(error);
+        if (error instanceof Error) {
+            const errorMessage = error.message;
+            return res
+            .status(400)
+            .json({ error: "Error executing query", message: errorMessage });
+        }
+        return res
+        .status(500)
+        .json({ error: "Internal server error", message: error });
+    }
+}
