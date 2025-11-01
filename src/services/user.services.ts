79187aeddbad7cd6659826948f47229f98eaa527
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-11-01 19:36:42 +0530

    error fixed

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -24,7 +24,7 @@ export const getAllUsers = async (
         };
     }
     catch (error) {
-        console.error("Error executing logout", error);
+        console.error("Error executing getAllUsers", error);
         return {
         statusCode: 500,
         message: "Internal server error",
