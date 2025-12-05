2c467273c28f105f664a02b22ba1bc7aeee7b174
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-05 23:58:37 +0530

    update types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,35 @@
 import { Decimal } from "@prisma/client/runtime/library";
+import { Request } from "express";
+
+export enum UserType {
+  CUSTOMER = "CUSTOMER",
+  STAFF = "STAFF"
+}
+
+enum UserRole {
+  ADMIN
+  MANAGER
+  OFFICER
+  CASHIER
+  WAITER
+  CHEF
+  STORE_KEEPER
+}
+
+enum ProfileType {
+  BRONZE
+  SILVER
+  GOLD
+}
+
+export interface AuthRequest extends Request {
+  user?: {
+    id: string;
+    email: string;
+    role?: UserRole;
+    profileType: ProfileType;
+  };
+}
 
 export enum ProfileType {
   BRONZE = "BRONZE",
@@ -27,91 +58,3 @@ export enum OrderStatus {
   READY = "READY",
   DELIVERED = "DELIVERED",
 }
-
-export type User = {
-  id: number;
-  userName: string;
-  name: string;
-  phone: string;
-  email?: string | null;
-  password: string;
-  dateOfBirth?: Date | null;
-  profileType: ProfileType;
-  refreshToken?: string;
-  role: Role;
-  createdAt: Date;
-  order: Order[];
-};
-
-export type Staff = {
-  id: number;
-  userName: string;
-  name: string;
-  phone: string;
-  email?: string | null;
-  password: string;
-  role: Role;
-  createdAt: Date;
-  order: Order[];
-};
-
-export type Table = {
-  id: number;
-  tableNumber: number;
-  status: TableStatus;
-  order: Order[];
-};
-
-export type Product = {
-  id: number;
-  name: string;
-  description?: string | null;
-  categoryId: number;
-  stock: number;
-  createdAt: Date;
-  priceListItems: PriceListItems[];
-  orderItems: OrderItems[];
-};
-
-export type Category = {
-  id: number;
-  name: string;
-  description: string;
-  createdAt: Date;
-  product: Product[];
-};
-
-export type PriceList = {
-  id: number;
-  name: string;
-  description?: string | null;
-  createdAt: Date;
-  priceListItems: PriceListItems[];
-};
-
-export type PriceListItems = {
-  id: number;
-  productId: number;
-  priceListId: number;
-  price: Decimal;
-};
-
-export type Order = {
-  id: number;
-  UserId?: number | null;
-  staffId?: number | null;
-  tableId?: number | null;
-  totalDiscount?: Decimal | null;
-  totalPrice: Decimal;
-  status: OrderStatus;
-  createdAt: Date;
-  orderItems: OrderItems[];
-};
-
-export type OrderItems = {
-  id: number;
-  orderId: number;
-  productId: number;
-  Quantity: number;
-  subTotal: Decimal;
-};
