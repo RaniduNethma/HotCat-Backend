a0d1a56fe1f91f95529d34b413323ad9cff25f49
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-06 21:20:56 +0530

    make authorizeRoles for role based authentication

---
+++ src/middlewares/authorize.middleware.ts
@@ -0,0 +1,20 @@
+import type { Response, NextFunction } from "express";
+import { AuthRequest, UserRole } from "../types/types.js";
+
+export const authorizeRoles = (...allowedRoles: UserRole[]) => {
+    return (req: AuthRequest, res: Response, next: NextFunction) => {
+        if(!req.user){
+            return res.status(401).json({error: 'Authentication required'});
+        }
+
+        if(!req.user.userRole || !allowedRoles.includes(req.user.userRole)){
+            return res.status(401).json({
+                error: 'Insufficient permissions',
+                required: allowedRoles,
+                current: req.user.userRole
+            });
+        }
+
+        next();
+    };
+};
