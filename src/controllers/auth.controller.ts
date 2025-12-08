3ed85527bee5a9c387b585fb5feb5b2bf68de901
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-08 19:20:06 +0530

    update register function in auth.controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,130 +1,126 @@
-import { Request, Response, NextFunction } from "express";
-import * as AuthServices from "../services/auth.services.js";
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "../types/types.js";
+import { AuthService } from "../services/auth.service.js";
 
-export const signup = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    //console.log("REQ BODY:", req.body);
-    const {userName, name, phone, email, dateOfBirth, password} = req.body;
+export class AuthController{
+  private authService: AuthService;
 
-    if(!userName || !password){
-      return res
-        .status(400)
-        .json({error: 'Username and password are required fields'});
-    }
-
-    const { statusCode, message, user} = await AuthServices.createUser(
-      userName, name, phone, email, dateOfBirth, password
-    );
-    
-    res.status(statusCode).json({message, user});
+  constructor() {
+    this.authService = new AuthService();
   }
-  catch (error) {
-    console.error(error);
-    if (error instanceof Error) {
-      const errorMessage = error.message;
-      return res
-        .status(400)
-        .json({ error: "Error executing query", message: errorMessage });
+
+  register = async (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const user = await this.authService.register(req.body);
+
+      res.status(201).json({
+        success: true,
+        data: user,
+        message: 'User registered successfully'
+      });
     }
-    return res
-      .status(500)
-      .json({ error: "Internal server error", message: error });
-  }
-};
-
-export const login = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { userName, password } = req.body;
-
-    if (!userName || !password) {
-      return res
-        .status(400)
-        .json({ error: "User name and password are required fields" });
+    catch (error: any) {
+      if (error.message === 'User already exists'){
+        return res.status(409).json({error: error.message});
+      }
+      next(error);
     }
+  };
 
-    const { statusCode, message, user, accessToken, refreshToken} =
-      await AuthServices.loginUser(userName, password);
+  export const login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const { userName, password } = req.body;
 
-    return res.status(statusCode).json({ user, message, accessToken, refreshToken});
-  }
-  catch (error) {
-    console.error(error);
-    if (error instanceof Error) {
-      const errorMessage = error.message;
-      return res
-        .status(400)
-        .json({ error: "Error executing query", message: errorMessage });
+      if (!userName || !password) {
+        return res
+          .status(400)
+          .json({ error: "User name and password are required fields" });
+      }
+
+      const { statusCode, message, user, accessToken, refreshToken} =
+        await AuthServices.loginUser(userName, password);
+
+      return res.status(statusCode).json({ user, message, accessToken, refreshToken});
     }
-    return res
-      .status(500)
-      .json({ error: "Internal server error", message: error });
-  }
-};
-
-export const refreshToken = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const token = req.body.refreshToken;
-    const newToken = await AuthServices.tokenGenerater(token);
-
-    return res
-      .status(200)
-      .json(newToken);
-  }
-  catch (error) {
-    console.error(error);
-    if (error instanceof Error) {
-      const errorMessage = error.message;
+    catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        const errorMessage = error.message;
+        return res
+          .status(400)
+          .json({ error: "Error executing query", message: errorMessage });
+      }
       return res
-        .status(400)
-        .json({ error: "Error executing query", message: errorMessage });
+        .status(500)
+        .json({ error: "Internal server error", message: error });
     }
-    return res
-      .status(500)
-      .json({ error: "Internal server error", message: error });
-  }
-}
+  };
 
-export const logout = async (
-  req: Request,
-  res: Response
-) => {
-  try {
-    const user = req.user as {id: number; role: string};
+  export const refreshToken = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const token = req.body.refreshToken;
+      const newToken = await AuthServices.tokenGenerater(token);
 
-    if(!req.user){
       return res
-        .status(401)
-        .json({message: 'Unauthorized'});
+        .status(200)
+        .json(newToken);
     }
-
-    const result = await AuthServices.logoutUser(user.id);
-
-    return res
-      .status(result.statusCode)
-      .json({message: result.message});
-  }
-  catch (error) {
-    console.error(error);
-    if (error instanceof Error) {
-      const errorMessage = error.message;
+    catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        const errorMessage = error.message;
+        return res
+          .status(400)
+          .json({ error: "Error executing query", message: errorMessage });
+      }
       return res
-        .status(400)
-        .json({ error: "Error executing query (Controller)", message: errorMessage });
+        .status(500)
+        .json({ error: "Internal server error", message: error });
     }
-    return res
-      .status(500)
-      .json({ error: "Internal server error", message: error });
+  }
+
+  export const logout = async (
+    req: Request,
+    res: Response
+  ) => {
+    try {
+      const user = req.user as {id: number; role: string};
+
+      if(!req.user){
+        return res
+          .status(401)
+          .json({message: 'Unauthorized'});
+      }
+
+      const result = await AuthServices.logoutUser(user.id);
+
+      return res
+        .status(result.statusCode)
+        .json({message: result.message});
+    }
+    catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        const errorMessage = error.message;
+        return res
+          .status(400)
+          .json({ error: "Error executing query (Controller)", message: errorMessage });
+      }
+      return res
+        .status(500)
+        .json({ error: "Internal server error", message: error });
+    }
   }
 }
