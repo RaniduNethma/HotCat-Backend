0e18a05afa127ef717d8926e7d306a5713adfaee
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-08 20:45:54 +0530

    update auth routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,35 @@
 import { Router } from "express";
-import * as authController from "../controllers/auth.controller.js";
-import { auth } from "../middlewares/auth.middleware.js";
+import { AuthController } from "../controllers/auth.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/validation.middleware.js";
+//import { authorizeRoles } from "../middlewares/authorize.middleware.js";
+import { z } from "zod";
 
 const authRouter = Router();
+const authController = new AuthController();
 
-authRouter.post("/signup", authController.signup);
-authRouter.post("/login", authController.login);
-authRouter.post("/token", authController.refreshToken);
-authRouter.post("/logout", auth(), authController.logout);
+const registerSchema = z.object({
+    userName: z.string().min(3),
+    name: z.string().min(3),
+    phone: z.string(),
+    email: z.string().optional(),
+    dateOfBirth: z.date().optional(),
+    userRole: z.enum(['ADMIN', 'MANAGER', 'OFFICER', 'CASHIER', 'WAITER', 'CHEF', 'STORE_KEEPER', 'CUSTOMER']),
+    password: z.string().min(6),
+    profileType: z.enum(['BRONZE', 'SILVER', 'GOLD']),
+    address: z.string().min(3).optional(),
+    city: z.string().optional()
+});
+
+const loginSchema = z.object({
+    userName: z.string().min(3),
+    password: z.string().min(6)
+});
+
+authRouter.post('/register', validate(registerSchema), authController.register);
+authRouter.post('/login', validate(loginSchema), authController.login);
+authRouter.post('/token', authenticate, authController.refreshToken);
+authRouter.post('/logout', authenticate, authController.logout);
+authRouter.get('/profile', authenticate, authController.getProfile);
 
 export default authRouter;
