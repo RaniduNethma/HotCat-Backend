df6e1ea4b85c9972404b36c794cf3ebb43383ecd
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-08 21:24:41 +0530

    format with prettier

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -9,27 +9,36 @@ const authRouter = Router();
 const authController = new AuthController();
 
 const registerSchema = z.object({
-    userName: z.string().min(3),
-    name: z.string().min(3),
-    phone: z.string(),
-    email: z.string().optional(),
-    dateOfBirth: z.date().optional(),
-    userRole: z.enum(['ADMIN', 'MANAGER', 'OFFICER', 'CASHIER', 'WAITER', 'CHEF', 'STORE_KEEPER', 'CUSTOMER']),
-    password: z.string().min(6),
-    profileType: z.enum(['BRONZE', 'SILVER', 'GOLD']),
-    address: z.string().min(3).optional(),
-    city: z.string().optional()
+  userName: z.string().min(3),
+  name: z.string().min(3),
+  phone: z.string(),
+  email: z.string().optional(),
+  dateOfBirth: z.date().optional(),
+  userRole: z.enum([
+    "ADMIN",
+    "MANAGER",
+    "OFFICER",
+    "CASHIER",
+    "WAITER",
+    "CHEF",
+    "STORE_KEEPER",
+    "CUSTOMER",
+  ]),
+  password: z.string().min(6),
+  profileType: z.enum(["BRONZE", "SILVER", "GOLD"]),
+  address: z.string().min(3).optional(),
+  city: z.string().optional(),
 });
 
 const loginSchema = z.object({
-    userName: z.string().min(3),
-    password: z.string().min(6)
+  userName: z.string().min(3),
+  password: z.string().min(6),
 });
 
-authRouter.post('/register', validate(registerSchema), authController.register);
-authRouter.post('/login', validate(loginSchema), authController.login);
-authRouter.post('/token', authenticate, authController.refreshToken);
-authRouter.post('/logout', authenticate, authController.logout);
-authRouter.get('/profile', authenticate, authController.getProfile);
+authRouter.post("/register", validate(registerSchema), authController.register);
+authRouter.post("/login", validate(loginSchema), authController.login);
+authRouter.post("/token", authenticate, authController.refreshToken);
+authRouter.post("/logout", authenticate, authController.logout);
+authRouter.get("/profile", authenticate, authController.getProfile);
 
 export default authRouter;
