fc763e9c1dc0eafae8b7e076d9d244d905180813
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-10 20:20:09 +0530

    update updateTable function in table.controller

--- src/controllers/table.controller.ts
+++ src/controllers/table.controller.ts
@@ -38,8 +38,7 @@ export class TableController {
 
   async getTableById(req: AuthRequest, res: Response, next: NextFunction) {
     try {
-      const id = req.params;
-      const table = await this.tableService.getTableById(Number(id));
+      const table = await this.tableService.getTableById(req.body);
 
       if (!table) {
         return res.status(404).json({ error: "Table not found" });
@@ -54,49 +53,20 @@ export class TableController {
     }
   }
 
-  updateTableHandler = async (req: Request, res: Response) => {
-    const { id, newTableNumber, newStatus } = req.body;
-
+  async updateTable(req: AuthRequest, res: Response, next: NextFunction) {
     try {
-      const { statusCode, message, data } = await tableServices.updateTable(
-        id,
-        newTableNumber,
-        newStatus
-      );
+      const table = await this.tableService.updateTable(req.body);
 
-      return res.status(statusCode).json({ message, data });
-    } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        const errorMessage = error.message;
-        return res
-          .status(400)
-          .json({ error: "Error executing query", message: errorMessage });
+      if (!table) {
+        return res.status(404).json({ error: "Table not found" });
       }
-      return res
-        .status(500)
-        .json({ error: "Internal server error", message: error });
-    }
-  };
 
-  deleteTablHandler = async (req: Request, res: Response) => {
-    const { id } = req.body;
-
-    try {
-      const { statusCode, message } = await tableServices.deleteTable(id);
-
-      return res.status(statusCode).json({ message });
+      return res.status(200).json({
+        success: true,
+        data: table,
+      });
     } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        const errorMessage = error.message;
-        return res
-          .status(400)
-          .json({ error: "Error executing query", message: errorMessage });
-      }
-      return res
-        .status(500)
-        .json({ error: "Internal server error", message: error });
+      next(error);
     }
-  };
+  }
 }
