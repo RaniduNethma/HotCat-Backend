ace5aea6a7200d7720e3d2139e734830ba3a0344
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-12 18:57:54 +0530

    update imports

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import app from './app.js';
-import { env } from './configs/envConfig.js';
+import app from "./app.js";
+import { env } from "./configs/envConfig.js";
 
 const port = env.SERVER_PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
