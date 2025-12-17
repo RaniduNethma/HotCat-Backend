f25dbd3a30f0fe48f5f423a79f13a1a23827a3b9
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-17 20:57:03 +0530

    specialize body, query and params in validations

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,19 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import { z } from 'zod';
+import { z } from "zod";
 
-export const validate = (schema: z.ZodSchema) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        try {
-            schema.parse(req.body);
-            next();
-        } catch (error) {
-            if(error instanceof z.ZodError){
-                return res.status(400).json({
-                    error: 'Validation failed',
-                    details: error
-                });
-            }
-            next(error);
-        }
-    };
-};
+export const validate =
+  (schema: z.ZodSchema) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation failed",
+          details: error,
+        });
+      }
+      next(error);
+    }
+  };
