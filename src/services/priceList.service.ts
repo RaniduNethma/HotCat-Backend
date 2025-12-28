95f179ab160e8c32f0da5634749a62678c28fa84
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-12-28 20:39:12 +0530

    create createPriceList service

---
+++ src/services/priceList.service.ts
@@ -0,0 +1,42 @@
+import DB from "../configs/dbConfig.js";
+import { CreatePriceListDTO, UpdatePriceListDTO } from "../types/types.js";
+
+export class PriceList {
+  async createPriceList(data: CreatePriceListDTO) {
+    const existingPriceList = await DB.priceList.findUnique({
+      where: { name: data.name },
+    });
+
+    if (existingPriceList) {
+      return {
+        statusCode: 409,
+        message: `PriceList name already exists`,
+      };
+    }
+
+    const priceList = await DB.priceList.create({
+      data: {
+        name: data.name,
+        description: data.description,
+        isActive: data.isActive,
+        isDefault: data.isDefault,
+        startDate: data.startDate,
+        endDate: data.endDate,
+
+        priceListItems: {
+          create: data.items.map((items) => ({
+            productId: items.productId,
+            price: items.price,
+          })),
+        },
+      },
+      include: { priceListItems: true },
+    });
+
+    return {
+      statusCode: 200,
+      message: "Create priceList successful",
+      data: priceList,
+    };
+  }
+}
