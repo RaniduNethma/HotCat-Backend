685a98a9f1f7dbd27bf31d58afc7ac8b7c6a17f2
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2026-01-07 19:25:40 +0530

    update validation with createOrderSchema in create order endpoint

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,10 +1,38 @@
 import { Router } from "express";
 import { OrderController } from "../controllers/order.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
+import z from "zod";
+import { validate } from "../middlewares/validation.middleware.js";
 
 const orderRouter = Router();
 const orderController = new OrderController();
 
-orderRouter.post("/create", authenticate, orderController.createOrder);
+const orderItemsSchema = z.object({
+  productId: z.number(),
+  Quantity: z.number(),
+  orderStatus: z.enum(["PENDING", "PREPARING", "READY", "SERVED"]),
+});
+
+const createOrderSchema = z.object({
+  body: z.object({
+    userId: z.number(),
+    tableId: z.number(),
+    assignedToId: z.number(),
+    orderStatus: z.enum(["PENDING", "PREPARING", "READY", "SERVED"]),
+    orderType: z.enum(["DINE_IN", "TAKEAWAY", "DELIVERY"]),
+    discount: z.number(),
+    paymentStatus: z.enum(["PENDING", "PAID", "PARTIALLY_PAID", "REFUNDED"]),
+    paymentMethod: z.string(),
+    completedAt: z.date(),
+    orderItems: z.array(orderItemsSchema),
+  }),
+});
+
+orderRouter.post(
+  "/create",
+  authenticate,
+  validate(createOrderSchema),
+  orderController.createOrder
+);
 
 export default orderRouter;
